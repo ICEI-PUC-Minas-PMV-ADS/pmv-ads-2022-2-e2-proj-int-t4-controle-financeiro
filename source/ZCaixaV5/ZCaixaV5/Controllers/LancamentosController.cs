29a603f3fa8b264eb4092bea63b90f8e3dac4788
Ricardo Teixeira <rjtsjr@gmail.com>
2022-12-06 12:00:33 -0300

    Totais e Controle de Meta Funcionais

--- source/ZCaixaV5/ZCaixaV5/Controllers/LancamentosController.cs
+++ source/ZCaixaV5/ZCaixaV5/Controllers/LancamentosController.cs
@@ -3,9 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 using Composite.Core.Linq;
 using Microsoft.Ajax.Utilities;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -43,10 +46,7 @@ namespace ZCaixaV5.Controllers
             {
                 Usuario usuario = await _contextusr.Usuarios.FindAsync(HttpContext.User.Identity.Name);
                 ViewBag.mesConsulta = usuario.MesConsulta;
-                ViewBag.anoConsulta = usuario.AnoConsulta;
-
-
-
+                ViewBag.anoConsulta = usuario.AnoConsulta;                
                 ViewBag.Nome = HttpContext.User.Identity.Name;
                 var categoria = from c in _contextcat.Categorias
                                 select c;
@@ -71,9 +71,99 @@ namespace ZCaixaV5.Controllers
                 var lancamento = from s in _context.Lancamentos.Include(s => s.Cat)
                                  select s;
                 lancamento = lancamento.Where(s => s.Username.Contains(HttpContext.User.Identity.Name));
-                lancamento = lancamento.Where(s => s.Data.Year == usuario.AnoConsulta);
-                lancamento = lancamento.Where(s => s.Data.Month == usuario.MesConsulta);
+                decimal totalReceitaG = 0;
+                decimal totalDespesaG = 0;
+                decimal totalSaldoG = 0;
+                decimal totalReceitaP = 0;
+                decimal totalDespesaP = 0;
+                decimal totalSaldoP = 0;
+                decimal totalMeta = 0;
+                int percMeta = 0;
+                foreach (Lancamento lanG in lancamento)
+                {
+                    if (lanG.Cat.Tipo == "R")
+                    {
+                        if (lanG.Tipo == "C")
+                        {
+                            totalReceitaG = totalReceitaG + lanG.Valor;
+                        }
+                        else if (lanG.Tipo == "D")
+                        {
+                            totalReceitaG = totalReceitaG - lanG.Valor;
+                        }
+                    }
+                    else if (lanG.Cat.Tipo == "D")
+                    {
+                        if (lanG.Tipo == "D")
+                        {
+                            totalDespesaG = totalDespesaG + lanG.Valor;
+                        }
+                        else if (lanG.Tipo == "C")
+                        {
+                            totalDespesaG = totalDespesaG - lanG.Valor;
+                        }
+                    }
+                };
+
+                if (usuario.AnoConsulta > 0)
+                {
+                    lancamento = lancamento.Where(s => s.Data.Year == usuario.AnoConsulta);
+                }
+                if (usuario.MesConsulta > 0)
+                {
+                    lancamento = lancamento.Where(s => s.Data.Month == usuario.MesConsulta);
+                }
                 lancamento = lancamento.OrderByDescending(s => s.Id).OrderByDescending(s => s.Data);
+                                
+                List<SelectListItem> grp = new List<SelectListItem>();
+                foreach (Lancamento lanP in lancamento)
+                {
+                    if (lanP.Cat.Tipo == "R")
+                    {
+                        if (lanP.Tipo == "C")
+                        {
+                            totalReceitaP = totalReceitaP + lanP.Valor;
+                        }
+                        else if (lanP.Tipo == "D")
+                        {
+                            totalReceitaP = totalReceitaP - lanP.Valor;
+                        }
+                    }
+                    else if (lanP.Cat.Tipo == "D")
+                    {
+                        if (lanP.Tipo == "D")
+                        {
+                            totalDespesaP = totalDespesaP + lanP.Valor;
+                        }
+                        else if (lanP.Tipo == "C")
+                        {
+                            totalDespesaP = totalDespesaP - lanP.Valor;
+                        }
+                    }
+
+                    //itens.Add(new SelectListItem { Text = w.Nome, Value = w.Id.ToString() });
+                }
+                totalSaldoG = totalReceitaG - totalDespesaG;
+                totalMeta = usuario.Meta;
+                if (totalMeta == 0)
+                {
+                    percMeta = 0;
+                }
+                else if (totalMeta < totalSaldoG)
+                {
+                    percMeta = 100;
+                }
+                else
+                {
+                    percMeta = (int)(totalSaldoG / totalMeta * 100);
+                }
+                totalSaldoP = totalReceitaP - totalDespesaP;
+                ViewBag.Receita = totalReceitaP;
+                ViewBag.Despesa = totalDespesaP; 
+                ViewBag.Saldo = totalSaldoP;
+                ViewBag.ValorMeta = totalMeta;
+                ViewBag.PercMeta = percMeta;
+
                 return View(await PaginaList<Lancamento>.CreateAsync(lancamento.AsNoTracking(), pageNumber ?? 1, pageSize));           
             }
         }
@@ -94,12 +184,42 @@ namespace ZCaixaV5.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> SairSistema()
+        {
+            var userClaims = new List<Claim>()
+                    {
+                        //define o cookie
+                        new Claim(ClaimTypes.Name, ""),
+                    };
+            var minhaIdentity = new ClaimsIdentity(userClaims, "usr");
+            var userPrincipal = new ClaimsPrincipal(new[] { minhaIdentity });
+            //cria o cookie
+            await HttpContext.SignInAsync(userPrincipal);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
         public async Task<JsonResult> PegarLancamento(int lancamentoId)
         {
             Lancamento lancamento = await _context.Lancamentos.FindAsync(lancamentoId);
             
             return Json(lancamento);
         }
+
+        [HttpPost]
+        public async Task<JsonResult> AtualizarMeta(string ValorMetaS)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuario usuario = await _contextusr.Usuarios.FindAsync(HttpContext.User.Identity.Name);
+                usuario.Meta = Convert.ToDecimal(ValorMetaS);
+                _contextusr.Usuarios.Update(usuario);
+                await _context.SaveChangesAsync();
+                return Json(ValorMetaS);
+            }
+            return Json(ModelState);
+        }
+
 
         [HttpPost]
         public async Task<JsonResult> AtualizarFiltroMes(int IDMes)
---
+++ source/ZCaixaV5/ZCaixaV5/Models/CategoriaListaValor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZCaixaV5.Models
+{
+    public class CategoriaListaValor
+    {
+        public int Tipo { get; set; }
+        public string Categoria { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Valor { get; set; }
+
+        public List<CategoriaListaValor> ListaCategoriasValor()
+        {
+            return new List<CategoriaListaValor>
+            {
+            };
+        }
+    }
+}
